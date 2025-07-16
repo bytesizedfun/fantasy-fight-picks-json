3354a66dbde134499486e4efaeef5fa318cb2832
bytesizedfun <joshbouw@gmail.com>
2025-07-16 12:19:30 -0400

    Update server.js

--- server.js
+++ server.js
@@ -58,4 +58,7 @@ app.get("/api/fights", (req, res) => {
   ]);
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
