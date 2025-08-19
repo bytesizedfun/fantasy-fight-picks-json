b7d9d4136e50b24f30a23cf782f8f4e9d9b47351
bytesizedfun <joshbouw@gmail.com>
2025-08-18 22:21:52 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
+// ✅ Polyfill for File/Blob/FormData (needed for node-fetch/undici on Node <20)
+const { File, Blob, FormData } = require("undici");
+globalThis.File ??= File;
+globalThis.Blob ??= Blob;
+globalThis.FormData ??= FormData;
+
 const express = require("express");
 const fetch = require("node-fetch");
 const path = require("path");
